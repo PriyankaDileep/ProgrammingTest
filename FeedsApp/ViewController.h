f97248130ba2adb75921082d7acf2e27589d1d20
Dileep <priyauthram@yahoo.co.in>
2018-05-03 14:12:59 +0930

    Initial Commit

---
+++ FeedsApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FeedsApp
+//
+//  Created by Dileep on 3/5/18.
+//  Copyright © 2018 Priyanka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FeedsApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FeedsApp
+//
+//  Created by Dileep on 3/5/18.
+//  Copyright © 2018 Priyanka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
