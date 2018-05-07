b85bd103204831036677e320e477298a553b133e
PriyankaDileep <38930377+PriyankaDileep@users.noreply.github.com>
2018-05-07 17:48:58 +0930

    Update FAFeed.m
    Comments Updated

--- FeedsApp/FAFeed.m
+++ FeedsApp/FAFeed.m
@@ -2,8 +2,8 @@
 //  FAFeed.m
 //  FeedsApp
 //
-//  Created by Dileep on 3/5/18.
-//  Copyright © 2018 Priyanka. All rights reserved.
+//  Created by Priyanka Dileep on 3/5/18.
+//  Copyright © 2018 Priyanka Dileep. All rights reserved.
 //
 
 #import "FAFeed.h"
