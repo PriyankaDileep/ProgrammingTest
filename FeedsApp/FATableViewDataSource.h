904a42053e0cb5139cbdf3fb5e46eb84706737fa
PriyankaDileep <38930377+PriyankaDileep@users.noreply.github.com>
2018-05-08 16:39:59 +0930

    Update FATableViewDataSource.h
    Comments updated

--- FeedsApp/FATableViewDataSource.h
+++ FeedsApp/FATableViewDataSource.h
@@ -2,8 +2,8 @@
 //  FATableViewDataSource.h
 //  FeedsApp
 //
-//  Created by Dileep on 3/5/18.
-//  Copyright © 2018 Priyanka. All rights reserved.
+//  Created by Priyanka Dileep  on 3/5/18.
+//  Copyright © 2018 Priyanka Dileep . All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
