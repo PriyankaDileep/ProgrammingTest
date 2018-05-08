fe2ea75d024e21179694bbeff04f922e5fd1e132
PriyankaDileep <38930377+PriyankaDileep@users.noreply.github.com>
2018-05-08 16:42:31 +0930

    Update NSString+Additions.m
    Comments Updated

--- FeedsApp/NSString+Additions.m
+++ FeedsApp/NSString+Additions.m
@@ -2,8 +2,8 @@
 //  NSString+Additions.m
 //  FeedsApp
 //
-//  Created by Dileep on 3/5/18.
-//  Copyright © 2018 Priyanka. All rights reserved.
+//  Created by Priyanka Dileep  on 3/5/18.
+//  Copyright © 2018 Priyanka Dileep . All rights reserved.
 //
 
 #import "NSString+Additions.h"
